33278ea6d5d7ab64032d7b016811b78c9cf42cc6
최진욱 <ultramarine@fasoo.com>
2022-09-26 20:32:16 +0900

    기본적으로 필요한 리소스 추가, 불필요한 주석 제거

---
+++ nonoflex_alpha/lib/gen/colors.gen.dart
@@ -0,0 +1,90 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #FFFFFF
+  static const Color base = Color(0xFFFFFFFF);
+
+  /// Color: #F1F4F8
+  static const Color baseDark = Color(0xFFF1F4F8);
+
+  /// Color: #000000
+  static const Color bbBlack = Color(0xFF000000);
+
+  /// Color: #E21F1F
+  static const Color bbRed = Color(0xFFE21F1F);
+
+  /// Color: #ffffff
+  static const Color bbWhite = Color(0xFFFFFFFF);
+
+  /// Color: #E21F1F
+  static const Color error = Color(0xFFE21F1F);
+
+  /// Color: #D43D3D
+  static const Color errorDisabled = Color(0xFFD43D3D);
+
+  /// Color: #1F6DE2
+  static const Color nonoBlue = Color(0xFF1F6DE2);
+
+  /// Color: #F96D39
+  static const Color nonoOrange = Color(0xFFF96D39);
+
+  /// Color: #FFA800
+  static const Color nonoYellow = Color(0xFFFFA800);
+
+  /// Color: #1F6DE2
+  static const Color primary = Color(0xFF1F6DE2);
+
+  /// Color: #0043AF
+  static const Color primaryDark = Color(0xFF0043AF);
+
+  /// Color: #1F6DE2
+  static const Color primaryDisabled = Color(0xFF1F6DE2);
+
+  /// Color: #6A9BFF
+  static const Color primaryLight = Color(0xFF6A9BFF);
+
+  /// Color: #E2F1FF
+  static const Color secondary = Color(0xFFE2F1FF);
+
+  /// Color: #B0BECC
+  static const Color secondaryDark = Color(0xFFB0BECC);
+
+  /// Color: #F2F9FF
+  static const Color secondaryLight = Color(0xFFF2F9FF);
+
+  /// Color: #000000
+  static const Color shadow = Color(0xFF000000);
+
+  /// Color: #1F6DE2
+  static const Color textColor = Color(0xFF1F6DE2);
+
+  /// Color: #0B005C
+  static const Color textColorDark = Color(0xFF0B005C);
+
+  /// Color: #2E2E2E
+  static const Color textDark = Color(0xFF2E2E2E);
+
+  /// Color: #E9ECEF
+  static const Color textDisable = Color(0xFFE9ECEF);
+
+  /// Color: #E21F1F
+  static const Color textError = Color(0xFFE21F1F);
+
+  /// Color: #7A7A7D
+  static const Color textHint = Color(0xFF7A7A7D);
+
+  /// Color: #FFFFFF
+  static const Color textLight = Color(0xFFFFFFFF);
+}
