bf10525e0d6c93170c047f2ce7c10b2b0a0d80d9
최진욱 <jinsrobot@naver.com>
2023-02-17 22:37:15 +0900

    불필요한 코드 제거

--- nonoflex_alpha/lib/conf/config.dart
+++ nonoflex_alpha/lib/conf/config.dart
@@ -3,21 +3,7 @@ import 'package:nonoflex_alpha/model/data/user.dart';
 
 class Configs {
   // 서버 설정 정보
-  static const serverAddress = 'nonoflex.com';
-  static const portNum = '3000';
-  static const version = 'v1';
 
-  // static const serverAddress = 'santa-house.tplinkdns.com';
-  // static const portNum = '3000';
-  // static const version = 'v1';
-
-  // static const serverAddress = '3.39.53.3';
-  // static const portNum = '3000';
-  // static const version = 'v1';
-
-  // static const serverAddress = 'api.nonoflex.com';
-  // static const portNum = '443';
-  // static const version = 'v1';
 
   // 토큰 정보
   String? accessToken;
